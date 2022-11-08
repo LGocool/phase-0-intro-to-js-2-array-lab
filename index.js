dcf37c21a30355ba719b8b3033561ef7454cf352
LGocool <67392650+LGocool@users.noreply.github.com>
2022-11-07 21:39:47 -0500

    commit tree d6a11bd81b968adaa064d0020fafaea98c0ec105

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+ const cats = ["Milo","Otis","Garfield"];
+
+ function destructivelyAppendCat(cat){
+    cats.push(cat);
+}
+
+function destructivelyPrependCat(cat){
+    cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat(cat){
+    cats.pop(cat);
+}
+
+function destructivelyRemoveFirstCat(cat){
+    cats.shift(cat);
+}
+
+const copyOfCats = [...cats];
+
+function appendCat(cat){
+    const copyOfCats = [...cats];
+        copyOfCats.push(cat);
+         return copyOfCats;
+}
+
+function prependCat(cat){
+    const copyOfCats = [...cats];
+        copyOfCats.unshift(cat);
+            return copyOfCats;
+}
+
+function removeLastCat(cat){
+    const copyOfCats = [...cats];
+        copyOfCats.pop(cat);
+         return copyOfCats;
+}
+
+function removeFirstCat(cat){
+    const copyOfCats = [...cats];
+        copyOfCats.shift(cat);
+            return copyOfCats;
+}
